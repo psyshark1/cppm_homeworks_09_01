50078380face9a3a293ac28c1c3ceb120a46cbb7
Andrey <andrey.zalozhenkov@pikta.ru>
2023-07-27 08:58:39 +0400

    Добавьте файлы проекта.

---
+++ 01.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+
+	bool operator==(Fraction& fr)
+	{
+		if (numerator_ == fr.numerator_ && denominator_ == fr.numerator_) return true;
+		return ((numerator_ * fr.denominator_) == (fr.numerator_ * denominator_));
+	}
+	bool operator!=(Fraction fr)
+	{
+		return ((numerator_ * fr.denominator_) != (fr.numerator_ * denominator_));
+	}
+	bool operator<(Fraction& fr)
+
+	{
+		if (denominator_ == fr.denominator_)
+		{
+			return (numerator_ > fr.numerator_);
+		}
+		if (numerator_ == fr.numerator_)
+		{
+			return (denominator_ > fr.denominator_);
+		}
+		return ((numerator_ * fr.denominator_) < (fr.numerator_ * denominator_));
+		
+	}
+	bool operator>(Fraction& fr)
+	{
+		if (denominator_ == fr.denominator_)
+		{
+			return (numerator_ > fr.numerator_);
+		}
+		if (numerator_ == fr.numerator_)
+		{
+			return (denominator_ > fr.denominator_);
+		}
+		return ((numerator_ * fr.denominator_) > (fr.numerator_ * denominator_));
+	}
+	bool operator<=(Fraction& fr)
+	{
+		if (denominator_ == fr.denominator_)
+		{
+			return (numerator_ > fr.numerator_);
+		}
+		if (numerator_ == fr.numerator_)
+		{
+			return (denominator_ > fr.denominator_);
+		}
+		return ((numerator_ * fr.denominator_) <= (fr.numerator_ * denominator_));
+	}
+	bool operator>=(Fraction& fr)
+	{
+		if (denominator_ == fr.denominator_)
+		{
+			return (numerator_ > fr.numerator_);
+		}
+		if (numerator_ == fr.numerator_)
+		{
+			return (denominator_ > fr.denominator_);
+		}
+		return ((numerator_ * fr.denominator_) >= (fr.numerator_ * denominator_));
+	}
+};
+
+int main()
+{
+
+	Fraction f1(5, 7);
+	Fraction f2(10, 14);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	std::system("pause");
+	return 0;
+}
